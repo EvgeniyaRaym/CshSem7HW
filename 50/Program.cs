10b854e071d22bdabc84d3412bb2ff83b622a3ea
EvgeniyaRaym <avariyanik@mail.ru>
2023-06-15 11:26:00 +0300

    first commit

---
+++ 50/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Write("Введите индекс строки: ");
+int x=int.Parse(ReadLine());
+Write("Введите индекс столбца: ");
+int y=int.Parse(ReadLine());
+Element(array, rows, columns, x , y);
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void Element (int [,] array, int rows, int columns, int x , int y)
+{
+    if (x<= columns && y<= rows)
+    WriteLine($"{array[x,y]}");   
+    else 
+    WriteLine("Такого числа в массиве нет");  
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
